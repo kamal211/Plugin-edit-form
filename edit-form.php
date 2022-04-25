69dcbffb957ee63d165330eec11794d99d31dde7
kamal211 <92024406+kamal211@users.noreply.github.com>
2022-04-25 20:23:35 +0000

    Update edit-form.php

--- edit-form.php
+++ edit-form.php
@@ -6,73 +6,38 @@
 </html>
 
 <?php
- /*
-Plugin Name: edit form
-Plugin URI: https://wordpress.com
-Author: Kamal&shaimae
-Author URI: https://wordpress.com
-*/
+/**
+ * Plugin Name:       form contact
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            kamal&shaimae
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+add_filter( 'show_admin_bar', '__return_false');
 
-function form_plugin()
-{
-        $form = '';
-        $form .='<h2>Contact us </h2>';
-        $form .= '<form method="post" action="http://localhost/wordpress/thanks/" >';
-        $form .='<label for="Name">Name </label>';
-        $form .='<input type="text" name="Name" class="form-control" placeholder="enter your name">';
-
-        $form .='<label for="Email">Email </label>';
-        $form .='<input type="email" name="Email" class="form-control" placeholder="enter your email">';
-
-        $form .='<label for="Phone number">Phone number </label>';
-        $form .='<input type="num" name="Phone-number" class="form-control" placeholder="enter your number">';
-
-        
-        $form .='<label for="Company">Company </label>';
-        $form .='<input type="text" name="Company" class="form-control" placeholder="enter your Company">';
-
-        
-        $form .='<label for="Subject">Subject </label>';
-        $form .='<input type="text" name="Subject" class="form-control" placeholder="enter your Subject">';
-
-        
-        $form .='<label for="Question">Question</label>';
-        $form .='<input type="text" name="Question" class="form-control" placeholder="enter your Question">';
-
-        $form .= '<br/> <input type="submit" name="form_submit" value="send your information" class="btn btn-md btn-primary ">';
-        $form .= '</form>';
-
-
-        
-        return $form;
-        
-
-}
-add_shortcode('short-form','form_plugin');
-
-function form_capture()
-{
-        if(isset($_POST['form_submit']))
-        {
-                // echo "<pre>";print_r($_POST);echo "</pre>";
-                $name = sanitize_text_field($_POST['Name']);
-                $Email = sanitize_text_field($_POST['Email']);
-                $number = sanitize_text_field($_POST['Phone-number']);
-                $company = sanitize_text_field($_POST['Company']);
-                $subject = sanitize_text_field($_POST['Subject']);
-                $question = sanitize_textarea_field($_POST['Question']);
-
-                $to = 'chaymaeboukhana.solicode@gmail.com';
-                $subject = 'Test form gha kanjrbo';
-                $message = ''.$name.' - '.$Email.' - '.$number.' - '.$company.' - '.$subject.' - '.$question;
-
-                wp_mail($to,$subject,$message);
-
-
-
-
+function donate_shortcode() {
+  return 'test shortcode';
         }
-}
-add_action('wp_head','form_capture'); 
+        add_shortcode('donate', 'donate_shortcode');
 
+add_action( 'admin_menu', 'wporg_options_page' );
+function wporg_options_page() {
+    add_menu_page(
+        'WPOrg',
+        'WPOrg Options',
+        'manage_options',
+        plugin_dir_path(__FILE__) . 'admin.php',
+        null,
+        'dashicons-admin-settings',
+        90
+    );
+}
 ?>
